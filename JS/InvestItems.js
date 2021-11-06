51de6c887dfe2a3b8a081cb2e1d6bd7cfba3df3a
TaikiTkayanagi <ikiat4@gmail.com>
2021-11-07 01:36:45 +0900

    JSに追加

---
+++ JS/InvestItems.js
@@ -0,0 +1,9 @@
+class InvestItem{
+  constructor(img, name, price, numberOfPossession, description){
+    this.img = img;
+    this.name = name;
+    this.price = price;
+    this.numberOfPossession = numberOfPossession;
+    this.description = description;
+  }
+}
---
+++ JS/UserInfo.js
@@ -0,0 +1,8 @@
+class UserInfo{
+  //日数は、UserInfoに適していないと考え入れない
+  constructor(name, age, money){
+    this.name = name;
+    this.age = age;
+    this.money = money
+  }
+}
